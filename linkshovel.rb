ec95796b798b9f7a54825bab607475a1a3e118db
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2019-10-23 15:45:05 +0100

    Now displays total sites and displays program title

--- linkshovel.rb
+++ linkshovel.rb
@@ -24,11 +24,17 @@ end
 for i in 0..BigDecimal::INFINITY
   begin
     puts "\e[H\e[2J"
+    puts "LinkShovel"
+    puts "‾‾‾‾‾‾‾‾‾‾"
     link_current=links.to_a.sample
+    puts "Total sites: "+links.to_a.length.to_s
+    puts
     puts "Visiting "+link_current
     puts
     puts "Found: "+get_links(link_current).to_s
     links.merge(get_links(link_current))
+    puts
+    
     File.open("links.txt", 'w') { |file| file.write(links.to_s) }
   rescue
     next
