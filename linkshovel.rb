ff4cb48d7bf4630c894c77aab6004378acfa1ecc
Daniel Bradbury <danieljspaniel@gmail.com>
2020-01-22 14:56:32 +0000

    Added config loading

--- linkshovel.rb
+++ linkshovel.rb
@@ -1,8 +1,8 @@
+puts "Initializing..."
 require 'nokogiri'
 require 'open-uri'
 require 'set'
 require 'bigdecimal'
-
 def get_links(url)
   Nokogiri::HTML(open(url).read).css("a").map do |link|
     if (href = link.attr("href"))
@@ -10,16 +10,26 @@ def get_links(url)
     end
   end.compact
 end
-links = Set["https://google.com", "https://tumblr.com", "https://twitter.com", "https://reddit.com", "https://wikipedia", "https://en.wikinews.org/wiki/Main_Page", "https://facebook.com"]
-# Checking connection
+links = Set[]
+init_links = []
+puts "Reading config..."
+config = File.read('config.html')
+Nokogiri::HTML(config).css("a").map do |link|
+  if (href = link.attr("href"))
+    init_links.append(href)
+  end
+end.compact
+init_links.each { |url|
+    links.merge(get_links(url))
+}
+puts "Testing connection..."
 begin
-  link_current=links.to_a.sample
-  get_links(link_current)
+  get_links("http://google.com")
 rescue
   puts "Connection failed, check your connection and try again"
   exit
 end
-
+puts "Starting Threads..."
 kill=0
 for i in 1..30
 Thread.new {
@@ -32,7 +42,10 @@ for i in 0..BigDecimal::INFINITY
   end
 end
 }
+puts "  Thread "+i.to_s+" ready!"
 end
+puts "Entering main loop..."
+puts "\e[H\e[2J"
 Thread.new {
 links_int=links.to_a.length.to_s
 for i in 0..BigDecimal::INFINITY
